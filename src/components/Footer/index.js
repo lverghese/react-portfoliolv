69e2f1e22a0f3c55fcdd85073d8951fe876d7cc9
lverghese <loganverghese@gmail.com>
2021-11-04 19:33:12 -0400

    footer init

---
+++ src/components/Footer/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+
+function Footer() {
+
+
+
+    return(
+        <footer className="mt-5">
+            <Container>
+                <Row>
+                    <Col className="p-0">
+                        Logan Verghese
+                    </Col>
+                    <Col>
+                        Made by Logan Verghese
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
