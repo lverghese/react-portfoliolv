721275f8a20ec3e5fef953294a2b5df685054c79
lverghese <loganverghese@gmail.com>
2021-11-04 18:50:18 -0400

    appjs initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Logan Verghese",
+      headerLinks: [
+        {title: 'Home', path: '/'},
+        {title: 'About', path: '/about'},
+        {title: 'Projects', path: '/projects'},
+        {title: 'Contact', path: '/contact'},
+      ],
+      home: {
+        title: "My React Portfolio",
+        subTitle: "Let's See How I Did",
+        text: "Check out my past projects!"
+      },
+      about: {
+        title: "My React Portfolio",
+        subTitle: "Let's See How I Did",
+        text: "Check out my past projects!"
+      },
+      projects: {
+        title: "My Project Portfolio"
+       
+      },
+      contact: {
+        title: "Hit Me Up"
+        
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Hello from react
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
