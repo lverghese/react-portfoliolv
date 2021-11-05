de5745b4623f839d7e741aff62d3fba15f579e85
lverghese <loganverghese@gmail.com>
2021-11-04 23:13:38 -0400

    about me page styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,25 @@
 import React from "react";
-
+import profilePic from '../../assets/images/profile.png'
 function About(props) {
 
     return(
-        <p>About Page Works</p>
+        <div className="header-wrapper">
+            <div className="main-info">
+                <img className="profile-pic" src={profilePic} alt="about-me"></img>
+                <h1>Logan Verghese</h1>
+                { props.title && <h1>{props.title}</h1>}
+                <div>
+                    <p>
+                        My name is Logan Verghese and I am currently a student in UCF's Coding 
+                        Boot Camp. In my last semester of my Associates Degree, I decided that
+                        software development is the career path I want to take and I will do anything 
+                        in my power to work towards that.
+                        
+                    </p>
+                </div>
+            </div>
+
+        </div>
 
     )
 }
